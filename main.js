67cee10265bb9c8f69162155ba482f568390a064
hoangkhumcoangiu <hoangqsqt98@gmail.com>
2023-05-18 09:44:14 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// var hoTen = 'Dinh Tien Hoang'; // khai bao bien hoten
+// var tuoi = 25;                 // Khai bao bien tuoi             
+// var diaChi = 'QN';              // khai bao biên diachi
+// setTimeout(function() {         // Dùng hàm setTimeoout để in ra màn hình console sau 2s
+//     console.log(hoTen);
+//     console.log(tuoi);
+//     console.log(diaChi);
+// }, 2000);
+
+var hoTen;
+var tuoi;
+hoTen = prompt ('moi nhap ho ten');
+tuoi = prompt ('moi nhap tuoi');
+console.log (hoTen);
+console.log (tuoi);
