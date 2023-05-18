2089ac70549c8f280124fc925c387b4838bd38aa
hoangkhumcoangiu <hoangqsqt98@gmail.com>
2023-05-18 19:52:58 +0700

    Add new project food

---
+++ bai1.js
@@ -0,0 +1,8 @@
+var hoTen = 'Dinh Tien Hoang'; // khai bao bien hoten
+var tuoi = 25;                 // Khai bao bien tuoi             
+var diaChi = 'QN';              // khai bao biên diachi
+setTimeout(function() {         // Dùng hàm setTimeoout để in ra màn hình console sau 2s
+    console.log(hoTen);
+    console.log(tuoi);
+    console.log(diaChi);
+}, 2000);
---
+++ bai2.js
@@ -0,0 +1,6 @@
+var hoTen;
+var tuoi;
+hoTen = prompt ('moi nhap ho ten');
+tuoi = prompt ('moi nhap tuoi');
+console.log (hoTen);
+console.log (tuoi);
